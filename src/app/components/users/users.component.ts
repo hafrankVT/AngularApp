01c6c5f5bfebad8e56cff1f1656f98a858c8ac35
Harry 'Alan' Frank, Jr <hfrank3@radford.edu>
2018-07-07 00:12:21 -0400

    Added usage of ngIf

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,51 +8,59 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  // tslint:disable-next-line:no-inferrable-types
+  showExtended: boolean = true;
+  // tslint:disable-next-line:no-inferrable-types
+  loaded: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
     console.log('init...');
-    // This is where you would initialize API calls and such, rather than the constructor which is for Properties
-    this.users = [
-      {
-        firstName: 'John',
-        lastName: 'Doe',
-        age: 30,
-        address: {
-          street: '50 Main Street',
-          city: 'Boston',
-          state: 'MA'}
-      },
-      {
-        firstName: 'Kevin',
-        lastName: 'Hart',
-        age: 34,
-        address: {
-          street: '20 School Street',
-          city: 'Boston',
-          state: 'MA'}
-      },
-      {
-        firstName: 'Karen',
-        lastName: 'Gillan',
-        age: 34,
-        address: {
-          street: 'The T.A.R.D.I.S',
-          city: 'London',
-          state: 'England'}
-      }
-    ];
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          age: 30,
+          address: {
+            street: '50 Main Street',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Kevin',
+          lastName: 'Hart',
+          age: 34,
+          address: {
+            street: '20 School Street',
+            city: 'Boston',
+            state: 'MA'
+          }
+        },
+        {
+          firstName: 'Karen',
+          lastName: 'Gillan',
+          age: 34,
+          address: {
+            street: 'The T.A.R.D.I.S',
+            city: 'London',
+            state: 'England'
+          }
+        }
+      ];
 
-    this.addUser({
-      firstName: 'Matt',
-      lastName: 'Smith',
-      age: 8675309,
-      address: {
-        street: 'The T.A.R.D.I.S',
-        city: 'London',
-        state: 'England'}
-    });
+      this.addUser({
+        firstName: 'Matt',
+        lastName: 'Smith',
+      });
+      this.loaded = true;
+    }, 3000);
+    // This is where you would initialize API calls and such, rather than the constructor which is for Properties
+
+
+    this.showExtended = true;
   }
 
   addUser(user: User) {
