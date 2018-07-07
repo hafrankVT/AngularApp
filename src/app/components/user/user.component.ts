b75012e15b0fdab98c9c68a992d521c37e5f9a25
Harry 'Alan' Frank, Jr <hfrank3@radford.edu>
2018-07-06 23:18:46 -0400

    Removed unneccessary, added User interface class

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../../models/User';
 
 @Component({
   selector: 'app-user',
@@ -15,44 +16,20 @@ import { Component } from '@angular/core';
 
 export class UserComponent {
   // Properties
-  firstName: string;
-  lastName: string;
-  age: number;
-  address;
-
-  foo: any;
-  hasKids: boolean;
-  numberArray: number[];
-  stringArray: string[];
-  mixedArray: any[];
-  myTuple: [string, number, boolean];
+  user: User;
 
   // Methods
   constructor() {
-    this.firstName = 'John';
-    this.lastName = 'Doe';
-    this.age = 30;
-    this.address = {
-      street: '50 Main Street',
-      city: 'Boston',
-      state: 'MA'
+    this.user = {
+      firstName: 'John',
+      lastName: 'Doe',
+      age: 30,
+      address: {
+        street: '50 Main Street',
+        city: 'Boston',
+        state: 'MA'
+      }
     };
-
-    this.foo = true;
-    this.hasKids = true;
-    this.numberArray = [1, 2, 3];
-    this.stringArray = ['hello', 'world'];
-    this.mixedArray = [true, 42, 'Hi!'];
-    this.myTuple = ['Hello', 13, false];
-
-    console.log(this.addNumbers(2, 3));
-  }
-
-  showAge() {
-    return this.age + 2;
-  }
-
-  addNumbers(num1: number, num2: number): number {
-    return num1 + num2;
   }
 }
+
